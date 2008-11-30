2771782240f1775927ed6ead180fdb70df64e5fd
justin@uberweiss.org <justin@justin-weisss-macbook-air.local>
2008-11-30 11:14:31 -0800

    Added a newline before the END token when no matches are found. This should make cases where there are no matches faster.

--- fuzzy-find-in-project.rb
+++ fuzzy-find-in-project.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 
 # Author: Justin Weiss
-# This is a simple binding to the fuzzy_file_finder gem. It really only makes
+# This is a simple wrapper for the fuzzy_file_finder gem. It really only makes
 # sense in the context of the fuzzy-find-in-project.el Emacs plugin. It takes
 # a query in stdin and returns a list of matching file names in stdout.
 # Usage: ./fuzzy-find-in-project.rb <project-path>
@@ -12,8 +12,13 @@ require 'fuzzy_file_finder'
 
 finder = FuzzyFileFinder.new(ARGV[0], 50000)
 while string = $stdin.readline
-  finder.find(string, 50).sort_by { |m| [-m[:score], m[:path]] }.each do |match|
-     puts "%s" % match[:path]
+  matches = finder.find(string, 50)
+  if matches && matches.length > 0
+    matches.sort_by { |m| [-m[:score], m[:path]] }.each do |match|
+      puts "%s" % match[:path]
+    end
+  else
+    puts
   end
   puts "END"
 end
